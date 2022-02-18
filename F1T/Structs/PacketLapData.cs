f4d0548942ba0f77c32748537732728e67c25583
ryanabaker <39451853+ryanabaker@users.noreply.github.com>
2022-02-17 20:46:04 -0500

    Add enums

--- F1T/Structs/PacketLapData.cs
+++ F1T/Structs/PacketLapData.cs
@@ -3,6 +3,53 @@ using System.Runtime.InteropServices;
 
 namespace F1T.Structs
 {
+    public enum PitStatus : byte
+    {
+        None,
+        Pitting,
+        InPit
+    }
+
+    public enum Sector : byte
+    {
+        SectorOne,
+        SectorTwo,
+        SectorThree
+    }
+
+    public enum CurrentLapInvalid : byte
+    {
+        Valid,
+        Invalid
+    }
+
+    public enum DriverStatus : byte
+    {
+        InGarage,
+        FlyingLap,
+        InLap,
+        OutLap,
+        OnTrack
+    }
+
+    public enum ResultStatus : byte
+    {
+        Invalid,
+        Inactive,
+        Active,
+        Finished,
+        DNF,
+        DSQ,
+        NotClassified,
+        Retired
+    }
+
+    public enum PitLaneTimerActive : byte
+    {
+        InActive,
+        Active
+    }
+
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct LapData
     {
@@ -17,21 +64,21 @@ namespace F1T.Structs
         public float m_safetyCarDelta;            // Delta in seconds for safety car
         public byte m_carPosition;             // Car race position
         public byte m_currentLapNum;       // Current lap number
-        public byte m_pitStatus;               // 0 = none, 1 = pitting, 2 = in pit area
+        public PitStatus m_pitStatus;               // 0 = none, 1 = pitting, 2 = in pit area
         public byte m_numPitStops;                 // Number of pit stops taken in this race
-        public byte m_sector;                  // 0 = sector1, 1 = sector2, 2 = sector3
+        public Sector m_sector;                  // 0 = sector1, 1 = sector2, 2 = sector3
         public byte m_currentLapInvalid;       // Current lap invalid - 0 = valid, 1 = invalid
         public byte m_penalties;               // Accumulated time penalties in seconds to be added
         public byte m_warnings;                  // Accumulated number of warnings issued
         public byte m_numUnservedDriveThroughPens;  // Num drive through pens left to serve
         public byte m_numUnservedStopGoPens;        // Num stop go pens left to serve
         public byte m_gridPosition;            // Grid position the vehicle started the race in
-        public byte m_driverStatus;            // Status of driver - 0 = in garage, 1 = flying lap
+        public DriverStatus m_driverStatus;            // Status of driver - 0 = in garage, 1 = flying lap
                                                // 2 = in lap, 3 = out lap, 4 = on track
-        public byte m_resultStatus;              // Result status - 0 = invalid, 1 = inactive, 2 = active
+        public ResultStatus m_resultStatus;              // Result status - 0 = invalid, 1 = inactive, 2 = active
                                                  // 3 = finished, 4 = didnotfinish, 5 = disqualified
                                                  // 6 = not classified, 7 = retired
-        public byte m_pitLaneTimerActive;          // Pit lane timing, 0 = inactive, 1 = active
+        public PitLaneTimerActive m_pitLaneTimerActive;          // Pit lane timing, 0 = inactive, 1 = active
         public ushort m_pitLaneTimeInLaneInMS;      // If active, the current time spent in the pit lane in ms
         public ushort m_pitStopTimerInMS;           // Time of the actual pit stop in ms
         public byte m_pitStopShouldServePen;   	 // Whether the car should serve a penalty at this stop
